a15d00f1628965a30ab3f4824802de676ec59f67
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2022-08-01 10:30:32 +0200

    ridechk functions added to ride.sh, code style change started

--- ride.sh
+++ ride.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ride.sh - Remove / Install / Disable / Enable 
+# ride.sh - Remove / Install / Disable / Enable
 #
 # Author: Torsten Juul-Jensen
 # July 30, 2022
@@ -11,17 +11,20 @@
 ###### Default Bootstrap Library Functions ###
 ################################################################
 
+# Function used across library files
 RequireAdmin(){
     # Function name if different from rest of script to match the library function names
     # check if script is root and restart as root if not
-    [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+    [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@" ; return 2> /dev/null; exit
 }
 
+# Function used across library files
 PressAnyKeyToContinue(){
     read -n 1 -s -r -p "Press any key to continue..."
     echo ""
 }
 
+# Function used across library files
 Restart(){
   echo Rebooting now...
   reboot now
@@ -31,14 +34,14 @@ Restart(){
 ###### Bootstrap Loader Functions (local only) ###
 ################################################################
 
-_help()
+usage_help()
 {
     SCRIPT_NAME=$(basename $0)
     echo "usage: $SCRIPT_NAME [--include <function library file>] [--preset <filename>] [[_]tweakname]"
     exit 1
 }
 
-_setVariables(){
+set_constants(){
 
   FEDORARELEASE=$(sed 's/[^0-9]//g' /etc/fedora-release) #Fedora release number
   DOWNLOADDIR=/tmp
@@ -53,13 +56,13 @@ _setVariables(){
   export SCRIPT_MYUSER=$MYUSER
   export SCRIPT_LOGINUSERUID=$LOGINUSERUID
   export SCRIPT_DOWNLOADDIR=$DOWNLOADDIR
-  export SCRIPT_MYUSERDIRE=$MYUSERDIR
+  export SCRIPT_MYUSERDIR=$MYUSERDIR
   export SCRIPT_WORKDIR
   export SCRIPT_VARSSET=1
 
 }
 
-_unsetVariables(){
+unset_constants(){
 
   unset SCRIPT_FEDORARELEASE
   unset SCRIPT_SCRIPTDIR
@@ -72,7 +75,7 @@ _unsetVariables(){
 
 }
 
-_logOutput(){
+log_output(){
   # Logs the output of the script to a log file
   # Redirect stdout and stderr to tee. Append all to LOGFILE
 
@@ -98,19 +101,19 @@ _logOutput(){
   echo Logfile:  $LOGFILE
   echo Script: $0
   echo Command line parameters: $@
-  echo "Timestamp: "$(date)
+  echo "Timestamp: $(date)"
   echo ""
   echo "################################################################"
   echo ""
 }
 
-_readPresetFile(){
-  #cat test.preset | cut -d '#' -f1 | grep -v -e '^[[:space:]]*$' -e '^#' | sed "s/\t\t*//g" | sed 's/ //g'
-  PRESETFILECONTENT=( `cat $1 | cut -d '#' -f1 | grep -v -e '^[[:space:]]*$' -e '^#' | sed "s/\t\t*//g" | sed 's/ //g' | sed 's/\r//g'` )
+read_preset_file(){
+  PRESETFILE=$1
+  PRESETFILECONTENT=( $(awk '{print $1}' ${PRESETFILE} | grep -v '^#' | awk 'NF') )
   ACTIONS=("${ACTIONS[@]}" "${PRESETFILECONTENT[@]}")
 }
 
-_addOrRemoveAction(){
+add_remove_action(){
   PARAMETER=$1
   if [[ ${PARAMETER:0:1} == "_" ]] ; then
     # If parameter value starts with an underscore "_" then exclude the action
@@ -124,18 +127,67 @@ _addOrRemoveAction(){
   fi
 }
 
-_functionExists() {
-    [ `type -t $1`"" == 'function' ]
+execute_functions() {
+    # execute all valid functions
+    echo "Performing: ${ACTIONS[@]}"
+    echo ""
+    for action in "${!ACTIONS[@]}"; do
+        ENTRYTYPE=$( type -t ${ACTIONS[action]} )
+        if [ x${ENTRYTYPE} == x'function' ]; then
+            echo "::: ${ACTIONS[$action]} :::"
+            (${ACTIONS[$action]})
+        else
+            echo "${action} is NOT a function - will not be executed"
+            continue
+        fi
+        echo ''
+    done
 }
 
-_executeFunctions(){
-  # execute all valid functions
-  for i in "${!ACTIONS[@]}"; do
-      _functionExists "${ACTIONS[$i]}" && echo "::: ${ACTIONS[$i]} :::" &&  (${ACTIONS[$i]}) # print name & execute function
+list_functions() {
+  PRESETFILEFIRSTLINE=$(grep -En "^[[:alnum:]]" $1 | head -n 1 | cut -d: -f1)
+  sed -n "${PRESETFILEFIRSTLINE},\$p" $1 | grep -i -E '^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)' | sed -e 's/[(){)]//g' | sed -e 's/function //I' | sed 's/\r$//g'
+}
+
+verbose_list_functions() {
+  PRESETFILEFIRSTLINE=$(grep -En "^[[:alnum:]]" $1 | head -n 1 | cut -d: -f1)
+  sed -n "${PRESETFILEFIRSTLINE},\$p" $1 | grep -i -E '^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)|###+' | sed -e 's/[(){)]//g' | sed -e 's/function //I' | sed 's/\r$//g' | grep -v -e '^##############'
+}
+
+#
+# Args: $1 is Function Library - $2 is preset file
+compare_library_preset() {
+  # preset file read
+  LIBFUNCTIONS=$(grep -i -E '^[[:space:]]*([[:alnum:]_]+[[:space:]]*\(\)|function[[:space:]]+[[:alnum:]_]+)' $1 | sed -e 's/[(){)]//g' | sed -e 's/function //I' | sed 's/\r$//g'  )
+  PRESETFILEFIRSTLINE=$(grep -En "^[[:alnum:]]" $2 | head -n 1 | cut -d: -f1)
+  PRESETFILECONTENT=$( sed -n "${PRESETFILEFIRSTLINE},\$p" $2 | grep -v -e '^##'  | sed 's/#/\n/g'| grep -v -e '^[[:space:]]*$' -e '^#'  | sed "s/\t\t*//g" | sed 's/ //g' | sed 's/\r$//g'  )
+
+  echo Preset functions from $2 missing in library file $1:
+  for PRESET in $PRESETFILECONTENT ; do
+    case "${LIBFUNCTIONS[@]}" in
+      *"$PRESET"*)
+        #echo -e "$PRESET - \e[1mOK\e[0m"
+        ;;
+      *)
+        echo -e "  $PRESET - \e[1m\e[31mMissing\e[0m"
+        ;;
+    esac
+  done
+
+  echo "${PRESETFILECONTENT[@]}" > /tmp/debug.txt
+  echo Library functions from $1 not in preset file $2:
+  for LIB in $LIBFUNCTIONS ; do
+    case "${PRESETFILECONTENT[@]}" in
+      *"$LIB"*)
+        #echo -e "$LIB - \e[1mOK\e[0m"
+        ;;
+      *)
+        echo -e "  $LIB - \e[1m\e[31mMissing\e[0m"
+    esac
   done
 }
 
-_parseArguments () {
+parse_arguments() {
     #declare global variable arrays
     declare -g -a PRESET # preset file(s)
     declare -g -a INCLUDE # library file(s)
@@ -145,17 +197,18 @@ _parseArguments () {
     LOGACTIONS=1 # controls whether script is logging all output to logfile
 
     if [[ $# -eq 0 ]] ; then
-      _help
+      usage_help
       exit 1
-    else
+    #else
       # check if script is root and restart as root if not
-      [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+      #[[ "$UID" -eq 0 ]] || exec sudo bash "$0" "$@" ; exit
     fi
 
       while [[ $# -gt 0 ]]
       do
         case $1 in
             -i | --include)
+                [[ "$UID" -eq 0 ]] || exec sudo bash "$0" "$@"
                 if [ -f $2 ] ; then
                   INCLUDE+="$2"
                   source $2 # Load script from file
@@ -169,12 +222,13 @@ _parseArguments () {
                 shift
                 ;;
             -p | --preset)
-              if [ -f $2 ] ; then
+              [[ "$UID" -eq 0 ]] || exec sudo bash "$0" "$@"
+              if [[ -f $2 ]] ; then
                 PRESET+="$2"
-                _readPresetFile $PRESET
-              elif [ -f $SCRIPTDIR/$2 ] ; then
+                read_preset_file $PRESET
+              elif [[ -f $SCRIPTDIR/$2 ]] ; then
                 PRESET+="$SCRIPTDIR/$2"
-                _readPresetFile "$SCRIPTDIR/$2"
+                read_preset_file "$SCRIPTDIR/$2"
               else
                 echo Preset file does not exist
                 exit 2
@@ -183,24 +237,59 @@ _parseArguments () {
               shift
               shift
               ;;
+            -l | --list )
+              LOGACTIONS=0
+              if [[ -f $2 ]] ; then
+                list_functions $2
+              elif [[ -f $SCRIPTDIR/$2 ]] ; then
+                list_functions "$SCRIPTDIR/$2"
+              else
+                echo Function library $2 was not found and could not be listed
+                usage_help
+              fi
+              exit 0
+              ;;
+            -c | --compare )
+              LOGACTIONS=0
+              if [[ -f $2 ]] && [[ -f $3 ]]; then
+                compare_library_preset $2 $3
+              else
+                echo "Compare command need two existing files as parameter (files not found)"
+                _help
+                exit 1
+              fi
+              exit 0
+              ;;
+            -v | --verbose_list )
+              LOGACTIONS=0
+              if [[ -f $2 ]] ; then
+                verbose_list_functions $2
+              elif [ -f $SCRIPTDIR/$2 ] ; then
+                verbose_list_functions "$SCRIPTDIR/$2"
+              else
+                echo "Verbose list command need an existing file as parameter (file not found)"
+                usage_help
+              fi
+              exit 0
+              ;;
             -n | --nolog)
               $LOGACTIONS=0
               ;;
             -h | --help )
-              _help
+              usage_help
               exit 1
               ;;
             * )
-            _addOrRemoveAction $1
-             shift
+            add_remove_action $1
+            shift
         esac
         #$i=$((i + 1))
     done
 }
 
 #### Main ####
-_setVariables $@
-_parseArguments $@
-(( $LOGACTIONS == 1)) && _logOutput $@
-_executeFunctions
-_unsetVariables
+set_constants $@
+parse_arguments $@
+[[ $LOGACTIONS == 1 ]] && log_output $@
+execute_functions
+unset_constants
